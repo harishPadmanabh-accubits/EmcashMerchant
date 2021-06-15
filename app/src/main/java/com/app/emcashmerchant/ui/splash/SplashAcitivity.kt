6c5be4b150b72f2935b417e96c4e88c4031f5b6d
harishpadmanabh-HP <harish.p@accubits.com>
2021-06-15 20:03:41 +0530

    add cta in splash

--- app/src/main/java/com/app/emcashmerchant/ui/splash/SplashAcitivity.kt
+++ app/src/main/java/com/app/emcashmerchant/ui/splash/SplashAcitivity.kt
@@ -2,11 +2,15 @@ package com.app.emcashmerchant.ui.splash
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.app.emcashmerchant.R
+import com.app.emcashmerchant.ui.login.LoginActivity
+import com.app.emcashmerchant.ui.register.RegisterActivity
 import com.app.emcashmerchant.utils.extensions.obtainViewModel
+import com.app.emcashmerchant.utils.extensions.openActivity
 
 
 class SplashAcitivity : AppCompatActivity() {
@@ -20,5 +24,13 @@ class SplashAcitivity : AppCompatActivity() {
 
     private fun getViewModel() {
         viewmodel = obtainViewModel(SplashViewmodel::class.java)
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            R.id.btn_login -> openActivity(LoginActivity::class.java)
+            R.id.btn_register -> openActivity(RegisterActivity::class.java)
+        }
+
     }
 }

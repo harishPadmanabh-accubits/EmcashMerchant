fc4d3251d4af5e3af569217abfdaaebf1e8fcdfa
harishpadmanabh-HP <harish.p@accubits.com>
2021-06-29 02:25:44 +0530

    add api setup

--- app/src/main/java/com/app/emcashmerchant/data/network/ApiServices.kt
+++ app/src/main/java/com/app/emcashmerchant/data/network/ApiServices.kt
@@ -1,4 +1,14 @@
 package com.app.emcashmerchant.data.network
 
+import com.app.emcashmerchant.data.models.SignupInitialRequestBody
+import com.app.emcashmerchant.data.models.SignupInitialResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface ApiServices {
+
+    @POST("v1/merchants/signup/initial")
+    fun performInitialSignup(@Body signupInitialRequestBody: SignupInitialRequestBody):Call<SignupInitialResponse>
+
 }
--- app/src/main/java/com/app/emcashmerchant/data/network/interceptors/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/app/emcashmerchant/data/network/interceptors/NetworkConnectionInterceptor.kt
@@ -5,6 +5,7 @@ import com.app.emcashmerchant.data.network.exceptions.NoInternetException
 import com.app.emcashmerchant.utils.CommonUtils
 import okhttp3.Interceptor
 import okhttp3.Response
+import timber.log.Timber
 import java.io.IOException
 
 class NetworkConnectionInterceptor (private val context: Context) : Interceptor {
@@ -12,6 +13,7 @@ class NetworkConnectionInterceptor (private val context: Context) : Interceptor 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         if(!CommonUtils.hasInternet(context)){
+            Timber.e("Throw no internet exception")
             throw NoInternetException()
         }
         val builder = chain.request().newBuilder()
